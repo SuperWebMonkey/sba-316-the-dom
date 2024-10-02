4bfd80e385f9ad393a171694fd42b7d88f8a8cdd
SuperWebMonkey <vikingsattack@outlook.com>
2024-10-01 22:57:35 -0700

    adding food item grid

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-import "./style.css";
+// import "./style.css";
 
 // testing if the js file is connected
-console.log("hello world");
+// console.log("hello world");
 
 // object of menu items, each category will contain 3-5 items
 const shopItems = {
   appetizers: [
     { name: "orange", price: 0.5 },
-    { name: "mandarin orange salse", price: 8.5 },
+    { name: "mandarin orange salsa", price: 8.5 },
     { name: "orange goat cheese bruschetta", price: 9.45 },
   ],
   entrees: [
@@ -22,14 +22,62 @@ const shopItems = {
   ],
 };
 
-// Creating a menu item form from the dom
-const contactEl = document.querySelector("#contact-container");
-contactEl.style.backgroundColor = "var(--contact-bg)";
+// Convert to an ary
+const menuAry = Object.keys(shopItems).map((key) => ({
+  key,
+  value: shopItems[key],
+}));
+
+// console.log(menuAry);
+
+// Get all the values of each object and put them into an array
+const itemAry = [];
+for (let i = 0; i < menuAry.length; i++) {
+  const itemObj = menuAry[i];
+  // console.log(itemObj);
+  const objAry = itemObj.value;
+  // Use the keys to iterate
+  // console.log(objAry);
+  for (let j = 0; j < objAry.length; j++) {
+    itemAry.push(objAry[j]);
+  }
+}
+// console.log(itemAry);
 
 // Create a chart of selectable pictures
-
-// function for cart - adding the cart items, showing the cart items,
+const menuItem = document.getElementById("menu");
+menuItem.style.width = "500px";
+// console.log(menuItem);
 
 // using bom alerts to get the username
-
 const submitBtn = document.getElementById("submit-btn");
+
+// Function to create the food grid in the html page
+function createItems(ary) {
+  ary.forEach((foodItem) => {
+    const foodBox = document.createElement("div");
+    foodBox.className = "food-box";
+
+    const foodName = document.createElement("h4");
+    foodName.textContent = foodItem.name;
+
+    const price = document.createElement("p");
+    price.textContent = `$${foodItem.price}`;
+
+    const button = document.createElement("button");
+    button.textContent = "order";
+    button.className = "order-button";
+
+    foodBox.appendChild(foodName);
+    foodBox.appendChild(price);
+    foodBox.appendChild(button);
+    menuItem.appendChild(foodBox);
+  });
+}
+
+createItems(itemAry);
+
+// click event for the button in the order menu
+const orderBtn = document.querySelector();
+
+// function for cart - adding the cart items, showing the cart items,
